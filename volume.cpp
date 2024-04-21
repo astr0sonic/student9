9d58d57b6a068a5cc48d8332e7d41da011fd93b0
sylvari <gizza_rina14@mail.ru>
2024-04-22 01:03:07 +0300

    task4

--- volume.cpp
+++ volume.cpp
@@ -2,24 +2,27 @@
 #include<algorithm>
 
 int getMaxVolume(int a, int b, int c, int n, int* arr) {
-    int maxvol = 0;
-    int left = 0;
-    int right = n - 1;
-    while (left < right)
-    {
-        int t = std::min(arr[left], arr[right]);
-        if ((right - left) <= c && t <= a)
-            t *= (right - left);
-        else
-            t = 0;
+	int maxvol = 0, left = 0, right = n - 1;
 
-        maxvol = std::max(maxvol, t);
+	while (left + a < n)
+	{
+		int m = std::min(arr[left], arr[left + a]);
+		if (m > maxvol && m <= c)
+			maxvol = m;
+		left++;
+	}
+	maxvol *= a;
 
-        if (arr[left] < arr[right])
-            left++;
-        else
-            right--;
+	while (left < right)
+	{
+		int t = std::min(arr[left], arr[right]);
+		if ((right - left) <= a && t <= c)
+			maxvol = std::max(maxvol, t * (right - left));
+		if (arr[left] < arr[right])
+			left++;
+		else
+			right--;
+	}
 
-    }
-    return maxvol * b;
+	return maxvol * b;
 }
