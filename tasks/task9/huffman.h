06040c23916d72e1cf1b27e3f0f6039b00ed49da
astrosonic <z.astrosonic@gmail.com>
2024-04-29 23:00:51 +0300

    edit task9

--- tasks/task9/huffman.h
+++ tasks/task9/huffman.h
@@ -1,5 +1,5 @@
 #include <string>
-#include <unordered_map>
+#include <map>
 
 std::string encode(const std::string& text);
-std::string decode(const std::string& encoded, std::unordered_map<char, std::string>& codes);
+std::string decode(const std::string& encoded, std::map<char, std::string>& codes);
